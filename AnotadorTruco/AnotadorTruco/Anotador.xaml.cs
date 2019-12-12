e69c0924f2d649ea83c06da7cded36cc13edf681
MateoMena <mateoccsj@gmail.com>
2019-12-12 19:31:28 -0300

    Examen 12/12

--- AnotadorTruco/AnotadorTruco/Anotador.xaml.cs
+++ AnotadorTruco/AnotadorTruco/Anotador.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,27 +18,33 @@ namespace AnotadorTruco
 			InitializeComponent ();
 
             
-           // lblEquipo1.Text = e1;
-
-           // lblEquipo2.Text = e2;
 		}
 
 
-        void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        void OnStepperValueChanged(object sender, ValueChangedEventArgs e, Entry labelEquipoNegro)
         {
             double value = e.NewValue;
             _rotatingLabel.Rotation = value;
            // _rotatingLabel2.Rotation = value;
             ContadorEquipo1.Text = string.Format("{0}", value);
-           // ContadorEquipo2.Text = string.Format("{0}", value);
+            if(value == 18)
+            {
+                DisplayAlert("Ganador",Convert.ToString(labelEquipoNegro), "Continuar");
+            }
         }
 
-        void OnStepperValueChanged2(object sender, ValueChangedEventArgs e)
+        void OnStepperValueChanged2(object sender, ValueChangedEventArgs e,Entry labelEquipoBlanco)
         {
             double value2 = e.NewValue;
             _rotatingLabel2.Rotation = value2;
             ContadorEquipo2.Text = string.Format("{0}", value2);
+            if (value2 == 18)
+            {
+                DisplayAlert("Ganador", Convert.ToString(labelEquipoBlanco), "Continuar");
+            }
         }
+
+        
 
     }
 }
